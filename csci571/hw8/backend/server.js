c793380c59d04b61d17bc8fbe14d2eb947cefbdb
liyuangang <1095350436@qq.com>
2023-03-17 13:36:07 -0700

    backend

--- csci571/hw8/backend/server.js
+++ csci571/hw8/backend/server.js
@@ -2,7 +2,8 @@ const express = require('express')
 const app = express()
 const axios = require('axios')
 const cors = require('cors')
-const geohash = require('ngeohash');
+const geohash = require('ngeohash')
+const SpotifyWebApi = require('spotify-web-api-node')
 
 app.use(cors({
     origin: '*'
@@ -10,6 +11,11 @@ app.use(cors({
 
 const TicketsMasterAPIKey = "Hi7gKkDkYdLmz9jNZOnvWveX8zfDYYSI"
 const GOOGLE_GEO_API_KEY = "AIzaSyDG0whBGCp_yx4zRwaIYyOFoVUHdhy9K-k"
+// credentials are optional
+const spotifyApi = new SpotifyWebApi({
+    clientId: 'a0e80f8b30514f55923ba98a4567f4e9',
+    clientSecret: 'c8fddba504184614ad0e704af15b18a7'
+})
 
 app.get('/', function (req, res) {
     res.send('Hello, World!')
@@ -92,7 +98,7 @@ const SegmentId = {
     "Music": "KZFzniwnSyZfZ7v7nJ",
     "Sports": "KZFzniwnSyZfZ7v7nE",
     "Arts & Theatre": "KZFzniwnSyZfZ7v7na",
-    "theatre" : "KZFzniwnSyZfZ7v7na",
+    "theatre": "KZFzniwnSyZfZ7v7na",
     "Film": "KZFzniwnSyZfZ7v7nn",
     "Miscellaneous": "KZFzniwnSyZfZ7v7n1"
 
@@ -103,10 +109,10 @@ app.get('/tickets', function (req, res) {
     let category = req.query.category
     let latitude = req.query.latitude
     let longitude = req.query.longitude
-    let url = "https://app.ticketmaster.com/discovery/v2/events?apikey="+TicketsMasterAPIKey
+    let url = "https://app.ticketmaster.com/discovery/v2/events?apikey=" + TicketsMasterAPIKey
     url += "&keyword=" + keyword
     url += "&radius=" + distance.toString() + "&unit=miles"
-    if (category != "Default"){
+    if (category != "Default") {
         url += "&segmentId=" + SegmentId[category]
     }
     url += "&geoPoint=" + geohash.encode(latitude, longitude, 7)
@@ -123,6 +129,64 @@ app.get('/tickets', function (req, res) {
     })
 
 })
+
+
+
+
+app.get('/event', function (req, res) {
+    let id = req.query.id
+    url = "https://app.ticketmaster.com/discovery/v2/events/"+id
+    axios.get(url, {
+        params: {
+            apikey: TicketsMasterAPIKey
+        }
+    }).then(response => {
+        let data = response.data
+        res.send(data)
+    }).catch(err => {
+        console.log(err)
+        res.send(
+            {
+                "error": err.toString()
+            })
+    })
+})
+
+
+
+app.get('/artistinfo', function (req, res) {
+    let artist = req.query.artist
+    spotifyApi.clientCredentialsGrant().then(
+        function (data) {
+            console.log('The access token is ' + data.body['access_token'])
+            spotifyApi.setAccessToken(data.body['access_token'])
+            spotifyApi.searchArtists(artist)
+                .then(function (data) {
+                    res.send(data.body)
+                }, function (err) {
+                    res.send(
+                        {
+                            "error": err.toString()
+                        })
+                })
+        },
+        function (err) {
+            res.send(
+                {
+                    "error": err.toString()
+                })
+        }
+    )
+
+
+})
+
+
+
+
+
+
+
 
 
 app.listen(8080, function () {
