3b69c6c7c10d562f5a32b06980b3008186182d6d
liyuangang <1095350436@qq.com>
2023-03-29 14:42:06 -0700

    AAAAAAAAAAAAAAAAAAA++++

--- csci571/hw8/frontend/event_search/server.js
+++ csci571/hw8/frontend/event_search/server.js
@@ -1,6 +1,3 @@
-// const express = require('express')
-// const app = express()
-// const path = require('path')
 const express = require('express')
 const cors = require('cors')
 const app = express()
@@ -9,17 +6,17 @@ app.use(cors({
     origin: '*'
 }))
 
-app.use(express.static(path.join(__dirname, 'dist/event_search')))
+app.use(express.static(path.join(__dirname, 'dist/event_search')));
 
 app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, 'dist/event_search/index.html'))
 })
-app.get('/search', (req, res) => {
-    res.sendFile(path.join(__dirname, 'dist/event_search/index.html'))
-})
-app.get('/favorites', (req, res) => {
-    res.sendFile(path.join(__dirname, 'dist/event_search/index.html'))
-})
+// app.get('/search', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'dist/event_search/index.html'))
+// })
+// app.get('/favorites', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'dist/event_search/index.html'))
+// })
 const port = process.env.PORT || 8080
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`)
